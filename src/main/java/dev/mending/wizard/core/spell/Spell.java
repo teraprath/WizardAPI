fcba518f22f0277b6c33d7f714434736973331f2
Terry <68700354+teraprath@users.noreply.github.com>
2024-11-10 21:42:22 +0100

    Spell Fix

--- src/main/java/dev/mending/wizard/core/spell/Spell.java
+++ src/main/java/dev/mending/wizard/core/spell/Spell.java
@@ -1,6 +1,5 @@
 package dev.mending.wizard.core.spell;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Consumer;
@@ -12,16 +11,14 @@ public class Spell {
     private final Consumer<Player> action;  // Die Aktion, die ausgeführt wird
 
     // Konstruktor, der sicherstellt, dass genau 3 Klicktypen übergeben werden
-    public Spell(Consumer<Player> action, ClickType... clickTypes) {
-        if (clickTypes.length != 3) {
-            throw new IllegalArgumentException("Exactly 3 ClickTypes must be provided.");
-        }
-
+    public Spell(Consumer<Player> action, ClickType c1, ClickType c2, ClickType c3) {
         this.action = action;
         this.combination = new ArrayList<>();
 
         // Füge die übergebenen Klicktypen zur Kombination hinzu
-        this.combination.addAll(Arrays.asList(clickTypes));
+        this.combination.add(c1);
+        this.combination.add(c2);
+        this.combination.add(c3);
     }
 
     // Aktion ausführen
